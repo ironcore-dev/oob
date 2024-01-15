90457d9ffde2127c77df6811b525384b26813149
Georgi Chulkov <georgi.chulkov@sap.com>
2024-01-15 15:51:40 +0100

    Add applyconfiguration

--- servers/console_server.go
+++ servers/console_server.go
@@ -39,7 +39,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	computev1alpha1 "github.com/onmetal/onmetal-api/api/compute/v1alpha1"
+	computev1alpha1 "github.com/ironcore-dev/ironcore/api/compute/v1alpha1"
 	"github.com/onmetal/oob-operator/internal/log"
 )
 
