df4f07947f200aaa4e5a88b5174d4033ac42be29
Damyan Yordanov <damyan@users.noreply.github.com>
2024-02-26 15:49:11 +0100

    Feature/reuse compliance (#121)
    * Add reuse skeleton – license headers, make targets, etc.
    
    * Update licenses, bulk configuration in `.reuse`
    
    * Add action for updating the reuse status badge

--- tools.go
+++ tools.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-License-Identifier: Apache-2.0
+
 //go:build tools
 // +build tools
 
@@ -11,6 +14,7 @@ import (
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/kustomize/v4"
 
+	_ "github.com/google/addlicense"
 	_ "github.com/ironcore-dev/ironcore/models-schema"
 	_ "github.com/ironcore-dev/vgopath"
 )
